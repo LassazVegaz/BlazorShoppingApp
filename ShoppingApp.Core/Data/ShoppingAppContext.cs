a17b0f802ff81de14aaab808bc730a236ec8a57e
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-12 11:14:04 +0530

    simple db context

---
+++ ShoppingApp.Core/Data/ShoppingAppContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Core.Models;
+
+namespace ShoppingApp.Core.Data;
+
+public class ShoppingAppContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+}
