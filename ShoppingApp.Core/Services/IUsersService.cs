9dc86c9faf9b05d54dd11a7a2b22e17153cb01fb
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-12 12:05:28 +0530

    users service with create user

---
+++ ShoppingApp.Core/Services/IUsersService.cs
@@ -0,0 +1,8 @@
+﻿using ShoppingApp.Core.Models;
+
+namespace ShoppingApp.Core.Services;
+
+public interface IUsersService
+{
+    User CreateUser(User newUser);
+}
---
+++ ShoppingApp.Logic/Services/UsersService.cs
@@ -0,0 +1,19 @@
+﻿using ShoppingApp.Core.Data;
+using ShoppingApp.Core.Models;
+using ShoppingApp.Core.Services;
+
+namespace ShoppingApp.Logic.Services;
+
+public class UsersService(ShoppingAppContext context) : IUsersService
+{
+    private readonly ShoppingAppContext _context = context;
+
+    public User CreateUser(User newUser)
+    {
+        newUser.Id = 0;
+        _context.Users.Add(newUser);
+        _context.SaveChanges();
+
+        return newUser;
+    }
+}
