f093f5eaab671ccab5ff2c739f35cd2a660b320c
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-13 12:42:52 +0530

    unique email constraint

--- ShoppingApp.Core/Data/ShoppingAppContext.cs
+++ ShoppingApp.Core/Data/ShoppingAppContext.cs
@@ -6,4 +6,14 @@ namespace ShoppingApp.Core.Data;
 public class ShoppingAppContext(DbContextOptions<ShoppingAppContext> options) : DbContext(options)
 {
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable(t => t.HasCheckConstraint("CK_unique_email", "UNIQUE(Email)"));
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
