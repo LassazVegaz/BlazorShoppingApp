ebf0f8fbb5db1052b782be99a381b52345535dc4
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-13 12:52:25 +0530

    unique email migrations

---
+++ ShoppingApp.Core/Migrations/20231213072137_UniqueEmail.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingApp.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_unique_email",
+                table: "Users",
+                sql: "UNIQUE(Email)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_unique_email",
+                table: "Users");
+        }
+    }
+}
