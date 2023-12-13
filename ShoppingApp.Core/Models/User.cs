d3fb6cb6d3468987fda607e2ee06cbd07c99ceb1
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-13 10:56:52 +0530

    first and last name in user model

--- ShoppingApp.Core/Models/User.cs
+++ ShoppingApp.Core/Models/User.cs
@@ -3,6 +3,8 @@
 public class User
 {
     public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
 }
