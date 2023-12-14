4ea3ceb7baa9e09f000a50ef750a4b7c34b2b5ac
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-14 17:36:28 +0530

    name fields added to UI

--- ShoppingApp/FormModels/RegisterModel.cs
+++ ShoppingApp/FormModels/RegisterModel.cs
@@ -11,4 +11,14 @@ class RegisterModel
     [Required]
     [MinLength(8, ErrorMessage = "Password must contain at least 8 characters")]
     public string Password { get; set; } = null!;
+
+    [Required]
+    [Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+    public string ConfirmPassword { get; set; } = null!;
+
+    [Required]
+    public string FirstName { get; set; } = null!;
+
+    [Required]
+    public string LastName { get; set; } = null!;
 }
