35c5aa6256edbe125d599f167ce4d00bebb3cf69
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-15 11:46:13 +0530

    async APIs

--- ShoppingApp.Core/Services/IUsersService.cs
+++ ShoppingApp.Core/Services/IUsersService.cs
@@ -4,10 +4,10 @@ namespace ShoppingApp.Core.Services;
 
 public interface IUsersService
 {
-    User CreateUser(User newUser);
+    Task<User> CreateUser(User newUser);
 
     /// <summary>
     /// Check if the email exists in the database
     /// </summary>
-    bool EmailExists(string email);
+    Task<bool> EmailExists(string email);
 }
--- ShoppingApp.Logic/Services/UsersService.cs
+++ ShoppingApp.Logic/Services/UsersService.cs
@@ -1,17 +1,20 @@
-﻿using ShoppingApp.Core.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Core.Data;
 using ShoppingApp.Core.Models;
 using ShoppingApp.Core.Services;
 
 namespace ShoppingApp.Logic.Services;
 
-public class UsersService(ShoppingAppContext context) : IUsersService
+public class UsersService(IDbContextFactory<ShoppingAppContext> contextFactory) : IUsersService
 {
-    private readonly ShoppingAppContext _context = context;
+    private readonly IDbContextFactory<ShoppingAppContext> _contextFactory = contextFactory;
 
-    public User CreateUser(User newUser)
+    public async Task<User> CreateUser(User newUser)
     {
         newUser.Id = 0;
-        _context.Users.Add(newUser);
+
+        using var _context = _contextFactory.CreateDbContext();
+        await _context.Users.AddAsync(newUser);
 
         // if the email already exists, an exception will be thrown from the DB side
         _context.SaveChanges();
@@ -19,5 +22,9 @@ public class UsersService(ShoppingAppContext context) : IUsersService
         return newUser;
     }
 
-    public bool EmailExists(string email) => _context.Users.Any(u => u.Email == email);
+    public Task<bool> EmailExists(string email)
+    {
+        using var _context = _contextFactory.CreateDbContext();
+        return _context.Users.AnyAsync(u => u.Email == email);
+    }
 }
