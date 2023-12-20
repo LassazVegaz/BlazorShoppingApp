7b464e6edb62fd0342aa0c360193c4d6d5d69708
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-20 18:57:39 +0530

    bs

---
+++ ShoppingApp/GlobalState/PageLoaderState.cs
@@ -0,0 +1,21 @@
+﻿namespace ShoppingApp.Web.GlobalState;
+
+public class PageLoaderState
+{
+    private bool _isLoading;
+
+    public event Action? OnChange;
+
+    public bool IsLoading
+    {
+        get => _isLoading;
+        set
+        {
+            if (_isLoading == value) return;
+            _isLoading = value;
+            NotifyStateChanged();
+        }
+    }
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
