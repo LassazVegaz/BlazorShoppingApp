7d810c2f2422c103915d75ecc1be05c40deb82b5
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-22 12:16:24 +0530

    signin basic UI

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,22 +1,75 @@
 import {
   Button,
   Container,
+  Grid,
   Stack,
   TextField,
   Typography,
-} from "@mui/material/index";
+} from "@mui/material";
 
 const SignInPage = () => {
   return (
-    <Container>
-      <Typography variant="h2">Sign In</Typography>
+    <Container sx={{ height: "100vh", display: "flex", alignItems: "center" }}>
+      <Grid
+        container
+        minHeight="85vh"
+        boxShadow="0px 0px 10px 0px rgba(0,0,0,0.75)"
+        borderRadius={2}
+        overflow="hidden"
+      >
+        <Grid
+          item
+          xs={4}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          bgcolor="primary.main"
+        >
+          <Typography
+            variant="h1"
+            fontSize={40}
+            textAlign="center"
+            fontWeight="bold"
+          >
+            Welcome Back!
+          </Typography>
 
-      <Stack>
-        <TextField label="Email" type="email" />
-        <TextField label="Password" type="password" />
-      </Stack>
+          <Typography variant="body1" fontSize={25} mt={2} textAlign="center">
+            Still don&apos;t have an account?
+          </Typography>
 
-      <Button>Sign In</Button>
+          <Button
+            variant="outlined"
+            sx={{
+              mt: 5,
+              color: "white",
+              borderColor: "white",
+            }}
+          >
+            Sign Up
+          </Button>
+        </Grid>
+
+        {/* form */}
+        <Grid
+          item
+          xs={8}
+          pl={5}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+        >
+          <Stack px={20} gap={2}>
+            <TextField label="Email" type="email" size="small" />
+            <TextField label="Password" type="password" size="small" />
+          </Stack>
+
+          <Stack alignItems="center" mt={5}>
+            <Button variant="contained">Sign In</Button>
+          </Stack>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
