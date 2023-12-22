a895b31bc2a791253b5240105ad012b9ef300def
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-22 12:55:34 +0530

    signup page styled

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -1,28 +1,31 @@
-import {
-  Button,
-  Container,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material/index";
+import AuthContainer from "@/components/AuthContainer";
+import { FormsFieldsContainer } from "@/components/AuthContainer/styled-components";
+import { Button, Stack, TextField } from "@mui/material/index";
 
 const SignUpPage = () => {
   return (
-    <Container>
-      <Typography variant="h2">Sign Up</Typography>
+    <AuthContainer
+      leftPanel={{
+        title: "Welcome!",
+        subtitle: "Already have an account?",
+        buttonText: "Sign In",
+        buttonLink: "/auth/signin",
+      }}
+    >
+      <FormsFieldsContainer>
+        <TextField label="First name" size="small" />
+        <TextField label="Last name" size="small" />
+        <TextField label="Email" type="email" size="small" />
+        <TextField label="Date of birth" type="date" size="small" />
+        <TextField label="Gender" size="small" />
+        <TextField label="Password" type="password" size="small" />
+        <TextField label="Confirm password" type="password" size="small" />
+      </FormsFieldsContainer>
 
-      <Stack>
-        <TextField label="First name" />
-        <TextField label="Last name" />
-        <TextField label="Email" type="email" />
-        <TextField label="Date of birth" type="date" />
-        <TextField label="Gender" />
-        <TextField label="Password" type="password" />
-        <TextField label="Confirm password" type="password" />
+      <Stack alignItems="center" mt={5}>
+        <Button variant="contained">Sign Up</Button>
       </Stack>
-
-      <Button>Sign Up</Button>
-    </Container>
+    </AuthContainer>
   );
 };
 
