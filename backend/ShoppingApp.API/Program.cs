e27a918eadcc394fae62f2e06a66b202d32aa358
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-22 22:02:08 +0530

    app services added to DIC

--- backend/ShoppingApp.API/Program.cs
+++ backend/ShoppingApp.API/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ShoppingApp.Core.Data;
+using ShoppingApp.Core.Services;
+using ShoppingApp.Logic.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,9 @@ builder.Services.AddSwaggerGen();
 var mySqlVersion = new MySqlServerVersion(builder.Configuration.GetValue("MySQLVersion", "8.0.29"));
 builder.Services.AddDbContext<ShoppingAppContext>(ops =>
     ops.UseMySql(builder.Configuration.GetConnectionString("ShoppingAppDb"), mySqlVersion));
+
+// app services
+builder.Services.AddScoped<IUsersService, UsersService>();
 
 var app = builder.Build();
 
