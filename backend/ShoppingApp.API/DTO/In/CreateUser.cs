a05d53e62cffe75c3fe004bc4078cebe9948e753
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-22 22:18:26 +0530

    user DTOs

---
+++ backend/ShoppingApp.API/DTO/In/CreateUser.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingApp.API.DTO.In;
+
+public class CreateUser
+{
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
---
+++ backend/ShoppingApp.API/DTO/Out/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoppingApp.API.DTO.Out;
+
+public class UserDto
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
