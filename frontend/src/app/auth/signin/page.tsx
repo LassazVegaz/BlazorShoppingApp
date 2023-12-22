92b2b99a01173b2a69ad6f6c0531191d80633fa6
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-22 12:51:14 +0530

    auth container into signin

--- frontend/src/app/auth/signin/page.tsx
+++ frontend/src/app/auth/signin/page.tsx
@@ -1,83 +1,26 @@
-import {
-  Button,
-  Container,
-  Grid,
-  Stack,
-  TextField,
-  Typography,
-} from "@mui/material";
-import Link from "next/link";
+import AuthContainer from "@/components/AuthContainer";
+import { FormsFieldsContainer } from "@/components/AuthContainer/styled-components";
+import { Button, Stack, TextField } from "@mui/material";
 
 const SignInPage = () => {
   return (
-    <Container sx={{ height: "100vh", display: "flex", alignItems: "center" }}>
-      <Grid
-        container
-        minHeight="85vh"
-        boxShadow="0px 0px 10px 0px rgba(0,0,0,0.75)"
-        borderRadius={2}
-        overflow="hidden"
-      >
-        {/* left panel */}
-        <Grid
-          item
-          xs={4}
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          bgcolor="primary.main"
-        >
-          <Typography
-            variant="h1"
-            fontSize={40}
-            textAlign="center"
-            fontWeight="bold"
-          >
-            Welcome Back!
-          </Typography>
+    <AuthContainer
+      leftPanel={{
+        title: "Welcome Back!",
+        subtitle: "Still don't have an account?",
+        buttonText: "Sign Up",
+        buttonLink: "/auth/signup",
+      }}
+    >
+      <FormsFieldsContainer>
+        <TextField label="Email" type="email" size="small" />
+        <TextField label="Password" type="password" size="small" />
+      </FormsFieldsContainer>
 
-          <Typography variant="body1" fontSize={25} mt={2} textAlign="center">
-            Still don&apos;t have an account?
-          </Typography>
-
-          <Link href="/auth/signup">
-            <Button
-              variant="outlined"
-              sx={{
-                mt: 5,
-                color: "white",
-                borderColor: "white",
-                "&:hover": {
-                  borderColor: "wheat",
-                },
-              }}
-            >
-              Sign Up
-            </Button>
-          </Link>
-        </Grid>
-
-        {/* form */}
-        <Grid
-          item
-          xs={8}
-          pl={5}
-          display="flex"
-          flexDirection="column"
-          justifyContent="center"
-        >
-          <Stack px={20} gap={2}>
-            <TextField label="Email" type="email" size="small" />
-            <TextField label="Password" type="password" size="small" />
-          </Stack>
-
-          <Stack alignItems="center" mt={5}>
-            <Button variant="contained">Sign In</Button>
-          </Stack>
-        </Grid>
-      </Grid>
-    </Container>
+      <Stack alignItems="center" mt={5}>
+        <Button variant="contained">Sign In</Button>
+      </Stack>
+    </AuthContainer>
   );
 };
 
