a02d94a6769a483e00653f41cebed935432a19e3
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-22 22:00:37 +0530

    db context service added to DIC

--- backend/ShoppingApp.API/Program.cs
+++ backend/ShoppingApp.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Core.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// db context
+var mySqlVersion = new MySqlServerVersion(builder.Configuration.GetValue("MySQLVersion", "8.0.29"));
+builder.Services.AddDbContext<ShoppingAppContext>(ops =>
+    ops.UseMySql(builder.Configuration.GetConnectionString("ShoppingAppDb"), mySqlVersion));
 
 var app = builder.Build();
 
