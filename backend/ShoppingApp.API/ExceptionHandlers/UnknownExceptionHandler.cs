b5a30de6f0caea3f7827da19928c8b05bf78fa52
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-23 13:14:22 +0530

    exception handling follows standards

--- backend/ShoppingApp.API/ExceptionHandlers/UnknownExceptionHandler.cs
+++ backend/ShoppingApp.API/ExceptionHandlers/UnknownExceptionHandler.cs
@@ -12,13 +12,25 @@ public class UnknownExceptionHandler(ILogger<UnknownExceptionHandler> logger, IW
     {
         _logger.LogError(exception, "An unknown error occurred.");
 
-        // message is not sent to the client in production
-        var msg = _env.IsDevelopment() ? exception.Message : null;
+        // Exception message is not sent to the client in production
+        var msg = _env.IsDevelopment()
+            ? exception.Message
+            : "An internal server error occured. Please contact support.";
+
+        // stack trace is sent as an extension in development
+        var extensions = _env.IsDevelopment()
+            ? new Dictionary<string, object?>()
+            {
+                [nameof(exception.StackTrace)] = exception.StackTrace,
+            } : [];
+
         var problemDetails = new ProblemDetails
         {
             Status = StatusCodes.Status500InternalServerError,
             Title = "An unknown error occurred.",
             Detail = msg,
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.6.1",
+            Extensions = extensions,
         };
 
         httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
