a64354914dc43c7fe63c895f5bdeee2c5f247db7
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-24 13:35:25 +0530

    refactor

--- backend/ShoppingApp.Logic/Services/UsersService.cs
+++ backend/ShoppingApp.Logic/Services/UsersService.cs
@@ -4,9 +4,9 @@ using ShoppingApp.Core.Services;
 
 namespace ShoppingApp.Logic.Services;
 
-public class UsersService(ShoppingAppContext contextFactory) : IUsersService
+public class UsersService(ShoppingAppContext context) : IUsersService
 {
-    private readonly ShoppingAppContext _context = contextFactory;
+    private readonly ShoppingAppContext _context = context;
 
     public async Task<bool> EmailExists(string email) => await _context.Users.AnyAsync(u => u.Email == email);
 }
