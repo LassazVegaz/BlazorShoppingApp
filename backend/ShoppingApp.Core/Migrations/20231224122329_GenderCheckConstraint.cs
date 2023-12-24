f6dcdef14ebb7af9f3f9c9516dd8fe42dc4d47c7
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-24 17:55:25 +0530

    gender migration

---
+++ backend/ShoppingApp.Core/Migrations/20231224122329_GenderCheckConstraint.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingApp.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class GenderCheckConstraint : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Gender",
+                table: "AspNetUsers",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_users_gender",
+                table: "AspNetUsers",
+                sql: "gender in ('male', 'female', 'other')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_users_gender",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "AspNetUsers");
+        }
+    }
+}
