0b84e3ba3ebf9eddb3f244faf4c6966b6ebc510d
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-24 18:04:30 +0530

    changed the most hated table names

--- backend/ShoppingApp.Core/Data/ShoppingAppContext.cs
+++ backend/ShoppingApp.Core/Data/ShoppingAppContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ShoppingApp.Core.Models;
 
@@ -15,40 +16,40 @@ public class ShoppingAppContext(DbContextOptions<ShoppingAppContext> options)
 
         builder.Entity<User>(b =>
         {
-            b.ToTable(t =>
+            b.ToTable("Users", t =>
             {
                 t.HasCheckConstraint("CK_users_gender", "gender in ('male', 'female', 'other')");
             });
         });
 
-        //builder.Entity<IdentityUserClaim<string>>(b =>
-        //{
-        //    b.ToTable("UserClaims");
-        //});
+        builder.Entity<IdentityUserClaim<string>>(b =>
+        {
+            b.ToTable("UserClaims");
+        });
 
-        //builder.Entity<IdentityUserLogin<string>>(b =>
-        //{
-        //    b.ToTable("UserLogins");
-        //});
+        builder.Entity<IdentityUserLogin<string>>(b =>
+        {
+            b.ToTable("UserLogins");
+        });
 
-        //builder.Entity<IdentityUserToken<string>>(b =>
-        //{
-        //    b.ToTable("UserTokens");
-        //});
+        builder.Entity<IdentityUserToken<string>>(b =>
+        {
+            b.ToTable("UserTokens");
+        });
 
-        //builder.Entity<IdentityRole>(b =>
-        //{
-        //    b.ToTable("Roles");
-        //});
+        builder.Entity<IdentityRole>(b =>
+        {
+            b.ToTable("Roles");
+        });
 
-        //builder.Entity<IdentityRoleClaim<string>>(b =>
-        //{
-        //    b.ToTable("RoleClaims");
-        //});
+        builder.Entity<IdentityRoleClaim<string>>(b =>
+        {
+            b.ToTable("RoleClaims");
+        });
 
-        //builder.Entity<IdentityUserRole<string>>(b =>
-        //{
-        //    b.ToTable("UserRoles");
-        //});
+        builder.Entity<IdentityUserRole<string>>(b =>
+        {
+            b.ToTable("UserRoles");
+        });
     }
 }
