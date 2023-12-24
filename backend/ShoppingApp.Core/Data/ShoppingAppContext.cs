8d879a4d70f783b42d468a9f4fbd5b5e42362dc0
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-24 17:45:08 +0530

    gender constraint

--- backend/ShoppingApp.Core/Data/ShoppingAppContext.cs
+++ backend/ShoppingApp.Core/Data/ShoppingAppContext.cs
@@ -1,8 +1,54 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ShoppingApp.Core.Models;
 
 namespace ShoppingApp.Core.Data;
 
-public class ShoppingAppContext(DbContextOptions<ShoppingAppContext> options) : IdentityDbContext(options)
+public class ShoppingAppContext(DbContextOptions<ShoppingAppContext> options)
+    : IdentityDbContext<User>(options)
 {
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // I hate aspnet{} names
+
+        builder.Entity<User>(b =>
+        {
+            b.ToTable(t =>
+            {
+                t.HasCheckConstraint("CK_users_gender", "gender in ('male', 'female', 'other')");
+            });
+        });
+
+        //builder.Entity<IdentityUserClaim<string>>(b =>
+        //{
+        //    b.ToTable("UserClaims");
+        //});
+
+        //builder.Entity<IdentityUserLogin<string>>(b =>
+        //{
+        //    b.ToTable("UserLogins");
+        //});
+
+        //builder.Entity<IdentityUserToken<string>>(b =>
+        //{
+        //    b.ToTable("UserTokens");
+        //});
+
+        //builder.Entity<IdentityRole>(b =>
+        //{
+        //    b.ToTable("Roles");
+        //});
+
+        //builder.Entity<IdentityRoleClaim<string>>(b =>
+        //{
+        //    b.ToTable("RoleClaims");
+        //});
+
+        //builder.Entity<IdentityUserRole<string>>(b =>
+        //{
+        //    b.ToTable("UserRoles");
+        //});
+    }
 }
