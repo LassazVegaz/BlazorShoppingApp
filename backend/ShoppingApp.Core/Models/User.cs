17923c2b87971d0d8ee64f8fa1c806fb43698ef7
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-24 17:44:02 +0530

    user model inherited from Identity

--- backend/ShoppingApp.Core/Models/User.cs
+++ backend/ShoppingApp.Core/Models/User.cs
@@ -1,10 +1,8 @@
-﻿namespace ShoppingApp.Core.Models;
+﻿using Microsoft.AspNetCore.Identity;
 
-public class User
+namespace ShoppingApp.Core.Models;
+
+public class User : IdentityUser
 {
-    public int Id { get; set; }
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
-    public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string Gender { get; set; } = null!;
 }
