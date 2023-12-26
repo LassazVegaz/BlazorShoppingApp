7ea872a8035563dba731f9c047f908071c3bf9ee
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-26 10:17:18 +0530

    fname & lname to createuser

--- backend/ShoppingApp.API/DTO/In/CreateUser.cs
+++ backend/ShoppingApp.API/DTO/In/CreateUser.cs
@@ -6,4 +6,6 @@ public class CreateUser
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
+    public string Gender { get; set; } = null!;
+    public DateTime DateOfBirth { get; set; }
 }
