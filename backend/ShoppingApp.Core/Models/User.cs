7d00548566621a31f6e9ae07c9bf4d5766ee9cf9
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-26 10:15:02 +0530

    firstname and lastname props to user model

--- backend/ShoppingApp.Core/Models/User.cs
+++ backend/ShoppingApp.Core/Models/User.cs
@@ -4,5 +4,8 @@ namespace ShoppingApp.Core.Models;
 
 public class User : IdentityUser
 {
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string Gender { get; set; } = null!;
     public DateOnly DateOfBirth { get; set; }
 }
