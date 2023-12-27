db08f26f48dcc49e07f2f9da7627b6adcb4af247
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-27 22:09:16 +0530

    auth login endpoint

---
+++ backend/ShoppingApp.API/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShoppingApp.Core.DTO.In;
+using ShoppingApp.Core.Services;
+
+namespace ShoppingApp.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController(IAuthService authService) : ControllerBase
+{
+    private readonly IAuthService _authService = authService;
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginCredentials request)
+    {
+        var token = await _authService.Login(request.Email, request.Password);
+        if (token is null) return Unauthorized("Invalid credentials");
+
+        Response.Cookies.Append("jwt", token, new CookieOptions
+        {
+            HttpOnly = true,
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTime.UtcNow.AddDays(7)
+        });
+
+        return Ok();
+    }
+}
