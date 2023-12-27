12ee7681aa553bef9ee58fdd7abcc7d1569c810b
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-27 19:09:36 +0530

    put create user params back

--- backend/ShoppingApp.API/DTO/In/CreateUser.cs
+++ backend/ShoppingApp.API/DTO/In/CreateUser.cs
@@ -2,10 +2,10 @@
 
 public class CreateUser
 {
-    public string? FirstName { get; set; } = "John";
-    public string? LastName { get; set; } = "Doe";
-    public string Email { get; set; } = null!;
-    public string? Password { get; set; } = "a";
-    public string? Gender { get; set; } = "male";
-    public DateOnly? DateOfBirth { get; set; } = new DateOnly(2000, 1, 1);
+    public string FirstName { get; set; } = default!;
+    public string LastName { get; set; } = default!;
+    public string Email { get; set; } = default!;
+    public string Password { get; set; } = default!;
+    public string Gender { get; set; } = default!;
+    public DateOnly DateOfBirth { get; set; }
 }
