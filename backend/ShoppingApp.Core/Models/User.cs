b462a9afd32d8c1259ee2a0c0232d1968dfa15de
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-27 17:47:43 +0530

    dob and gender - gender constraint also

--- backend/ShoppingApp.Core/Models/User.cs
+++ backend/ShoppingApp.Core/Models/User.cs
@@ -7,4 +7,6 @@ public class User
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
     public string Password { get; set; } = null!;
+    public string Gender { get; set; } = null!;
+    public DateOnly DateOfBirth { get; set; }
 }
