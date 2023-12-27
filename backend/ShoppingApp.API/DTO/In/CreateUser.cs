dd828281ec0e1dcaf143d234439486d515a8078b
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-27 18:54:20 +0530

    create user params made easy for testing

--- backend/ShoppingApp.API/DTO/In/CreateUser.cs
+++ backend/ShoppingApp.API/DTO/In/CreateUser.cs
@@ -2,8 +2,10 @@
 
 public class CreateUser
 {
-    public string FirstName { get; set; } = null!;
-    public string LastName { get; set; } = null!;
+    public string? FirstName { get; set; } = "John";
+    public string? LastName { get; set; } = "Doe";
     public string Email { get; set; } = null!;
-    public string Password { get; set; } = null!;
+    public string? Password { get; set; } = "a";
+    public string? Gender { get; set; } = "male";
+    public DateTime? DateOfBirth { get; set; } = new DateTime(2000, 1, 1);
 }
