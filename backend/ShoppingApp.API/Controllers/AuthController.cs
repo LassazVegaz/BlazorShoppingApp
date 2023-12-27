7824369d61f237a6beba8942bf442cc60c846b39
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-27 22:49:39 +0530

    get profile protected

--- backend/ShoppingApp.API/Controllers/AuthController.cs
+++ backend/ShoppingApp.API/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ShoppingApp.Core.DTO.In;
 using ShoppingApp.Core.Services;
 
@@ -28,6 +29,7 @@ public class AuthController(IAuthService authService, IUsersService usersService
     }
 
     [HttpGet("profile")]
+    [Authorize]
     public async Task<ActionResult> GetLoggedInUser()
     {
         var userId = User.Identity!.Name!;
