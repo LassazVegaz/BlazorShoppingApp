fe3a7ef1a1eca15c8539914b7eea556b5d0a9d29
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-27 17:33:12 +0530

    some useful comments

--- backend/ShoppingApp.API/Controllers/UsersController.cs
+++ backend/ShoppingApp.API/Controllers/UsersController.cs
@@ -16,31 +16,18 @@ public class UsersController(IUsersService usersService, IMapper mapper) : Contr
     [HttpPost]
     public async Task<ActionResult> CreateUser(CreateUser newUser)
     {
-        try
-        {
-            var newUserMapped = _mapper.Map<User>(newUser);
-            await _usersService.CreateUser(newUserMapped);
+        var newUserMapped = _mapper.Map<User>(newUser);
+        await _usersService.CreateUser(newUserMapped);
 
-            // this endpoint does not provide the created user's details because
-            // there is no endpoint get a user by id
-            return Created();
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        // there is no endpoint to get the created user's details because
+        // there is no endpoint get a user by id
+        // only logged in user can get their own details
+        return Created();
     }
 
     [HttpGet("emailExists/{email}")]
     public async Task<ActionResult<bool>> EmailExists(string email)
     {
-        try
-        {
-            return await _usersService.EmailExists(email);
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(ex.Message);
-        }
+        return await _usersService.EmailExists(email);
     }
 }
