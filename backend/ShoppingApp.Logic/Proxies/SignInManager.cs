8800c21cca1ecc735b916178b4c66e0e3f3f1b98
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-27 10:17:28 +0530

    gen token func

---
+++ backend/ShoppingApp.Logic/Proxies/SignInManager.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using ShoppingApp.Core.Models;
+using ShoppingApp.Core.Options;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ShoppingApp.Logic.Proxies;
+
+public class SignInManager(IOptions<JwtOptions> jwtOptions, UserManager<User> userManager, IHttpContextAccessor contextAccessor, IUserClaimsPrincipalFactory<User> claimsFactory, IOptions<IdentityOptions> optionsAccessor, ILogger<SignInManager<User>> logger, IAuthenticationSchemeProvider schemes, IUserConfirmation<User> confirmation)
+    : SignInManager<User>(userManager, contextAccessor, claimsFactory, optionsAccessor, logger, schemes, confirmation)
+{
+    private readonly JwtOptions _jwtOptions = jwtOptions.Value;
+
+    /// <summary>
+    /// Generate a JWT token for the user
+    /// </summary>
+    public async Task<string> GenerateTokenAsync(User user)
+    {
+        List<Claim> claims = [new(ClaimTypes.NameIdentifier, user.Id)];
+
+        var roles = await UserManager.GetRolesAsync(user);
+        foreach (var role in roles)
+            claims.Add(new Claim(ClaimTypes.Role, role));
+
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Secret));
+
+        var token = new JwtSecurityToken(issuer: _jwtOptions.Issuer,
+                                         audience: _jwtOptions.Audience,
+                                         claims: claims,
+                                         expires: DateTime.Now.AddDays(2),
+                                         signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256));
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
