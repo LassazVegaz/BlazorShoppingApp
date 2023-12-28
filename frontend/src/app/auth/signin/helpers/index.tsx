9717c226bccf96539d041f7a78ecd4f792d33c5c
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-28 21:52:12 +0530

    password regex is not needed in signin

--- frontend/src/app/auth/signin/helpers/index.tsx
+++ frontend/src/app/auth/signin/helpers/index.tsx
@@ -9,10 +9,5 @@ export const formDefaultValues = {
 
 export const validators = {
   email: Yup.string().email("Email is invalid").required("Required"),
-  password: Yup.string()
-    .required("Required")
-    .matches(
-      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[a-zA-Z\d@$!%*?&]{8,}$/,
-      "Password must be at least 8 characters long and contain at least one number, one uppercase letter, one lowercase letter and one special character from @$!%*?&"
-    ),
+  password: Yup.string().required("Required"),
 };
