1d07a0100564c8c96b82b78692d30c4b01f68161
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-28 09:26:00 +0530

    usefull comments

--- backend/ShoppingApp.API/Mapper/MapperProfile.cs
+++ backend/ShoppingApp.API/Mapper/MapperProfile.cs
@@ -10,7 +10,11 @@ public class MapperProfile : Profile
     public MapperProfile()
     {
         // source first, destination second
+
+        // sending to client
         CreateMap<User, UserDto>();
+
+        // receiving from client
         CreateMap<CreateUser, User>();
     }
 }
