d83327f0d08f329220f691b8637d87d14f1c615a
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-28 09:16:10 +0530

    return token for now

--- backend/ShoppingApp.API/Controllers/AuthController.cs
+++ backend/ShoppingApp.API/Controllers/AuthController.cs
@@ -18,14 +18,7 @@ public class AuthController(IAuthService authService, IUsersService usersService
         var token = await _authService.Login(request.Email, request.Password);
         if (token is null) return Unauthorized("Invalid credentials");
 
-        Response.Cookies.Append("jwt", token, new CookieOptions
-        {
-            HttpOnly = true,
-            SameSite = SameSiteMode.Strict,
-            Expires = DateTime.UtcNow.AddDays(7)
-        });
-
-        return Ok();
+        return Ok(token);
     }
 
     [HttpGet("profile")]
