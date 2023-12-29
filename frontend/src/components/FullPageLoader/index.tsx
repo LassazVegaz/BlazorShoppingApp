0198d1002527cdf777731bae52ca643d1520804d
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-29 19:33:02 +0530

    page loader bounded to loading state

--- frontend/src/components/FullPageLoader/index.tsx
+++ frontend/src/components/FullPageLoader/index.tsx
@@ -1,12 +1,15 @@
 "use client";
+import { useAppSelector } from "@/redux/hooks";
 import { Backdrop, CircularProgress } from "@mui/material";
 
 /**
  * Screen-wide global loader
  */
 const FullPageLoader = () => {
+  const open = useAppSelector((s) => s.pageLoader.loading);
+
   return (
-    <Backdrop open={false}>
+    <Backdrop open={open}>
       <CircularProgress
         size={60}
         thickness={5}
