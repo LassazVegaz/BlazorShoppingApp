360055b43e31ffa1e4e7fe035cd80d4cf1dadee6
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-30 16:10:30 +0530

    get user profile API

---
+++ frontend/src/dto/in/user.dto.ts
@@ -0,0 +1,10 @@
+type UserDto = {
+  id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  gender: string;
+  dateOfBirth: string;
+};
+
+export default UserDto;
--- frontend/src/lib/auth-api.ts
+++ frontend/src/lib/auth-api.ts
@@ -1,5 +1,6 @@
 import apiRoutes from "@/api-routes.json";
 import axios from "./axios";
+import UserDto from "@/dto/in/user.dto";
 
 const login = async (email: string, password: string) => {
   const response = await axios.post<string>(apiRoutes.auth.login, {
@@ -9,8 +10,14 @@ const login = async (email: string, password: string) => {
   return response.data;
 };
 
+const getProfile = async () => {
+  const response = await axios.get<UserDto>(apiRoutes.auth.profile);
+  return response.data;
+};
+
 const authApi = {
   login,
+  getProfile,
 };
 
 export default authApi;
