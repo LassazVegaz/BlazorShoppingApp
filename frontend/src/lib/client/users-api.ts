f9407234197c1ad75df35a1f4d91a6a88bd5476c
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-30 21:14:54 +0530

    update user api func

--- frontend/src/lib/client/users-api.ts
+++ frontend/src/lib/client/users-api.ts
@@ -1,6 +1,8 @@
 import apiRoutes from "@/api-routes.json";
 import clientAxios from "./client-axios";
 import CreateUser from "@/dto/out/create-user";
+import UpdateUser from "@/dto/out/update-user";
+import UserDto from "@/dto/in/user.dto";
 
 const emailExists = async (email: string) => {
   const response = await clientAxios.get<boolean>(
@@ -13,9 +15,15 @@ const createUser = async (data: CreateUser) => {
   await clientAxios.post(apiRoutes.users.common, data);
 };
 
+const updateUser = async (data: UpdateUser) => {
+  const res = await clientAxios.put<UserDto>(apiRoutes.users.common, data);
+  return res.data;
+};
+
 const usersApi = {
   emailExists,
   createUser,
+  updateUser,
 };
 
 export default usersApi;
