5079cf6f6e5b304ce9a870b772664c5abaaecf5e
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-30 16:29:01 +0530

    server axios

---
+++ frontend/src/lib/server/server-axios.ts
@@ -0,0 +1,22 @@
+import _axios from "axios";
+import { getToken } from "./server-tokens";
+
+const serverAxios = _axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+serverAxios.interceptors.request.use(
+  (config) => {
+    const token = getToken();
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default serverAxios;
