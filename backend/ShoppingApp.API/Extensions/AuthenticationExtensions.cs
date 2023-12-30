bb5259baa390f87aa227787e3d04a6351fcd8d97
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-30 12:22:50 +0530

    enabled cookie token validation too

--- backend/ShoppingApp.API/Extensions/AuthenticationExtensions.cs
+++ backend/ShoppingApp.API/Extensions/AuthenticationExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 using ShoppingApp.API.Constants;
 using ShoppingApp.Core.Options;
@@ -29,6 +30,32 @@ internal static class AuthenticationExtensions
                         ValidIssuer = jwtOptions.Issuer,
                         ValidAudience = jwtOptions.Audience,
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.Secret))
+                    };
+
+                    ops.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = async context =>
+                        {
+                            if (context.SecurityToken is not JsonWebToken headerToken)
+                                throw new Exception("Invalid token implementation");
+
+                            context.HttpContext.Request.Cookies.TryGetValue(CookieNames.JwtToken, out var cookieToken);
+                            if (cookieToken is null) return;
+                            else if (headerToken.EncodedToken != cookieToken) context.Fail("CSRF detected");
+
+                            // Note: this API allows authorization via cookie or header
+                            // It is recomemnded to use cookie when possible
+                            // If cookie is used, JWT should be in Authorization header as well to prevent CSRF
+                            // Either way, the token is in the header
+                            // Therefore let the libray validate the token in the header
+                            // That is why OnTokenValidated event is used here
+                            // If token is in cookie too, the client is using cookie based authentication
+                            // In that case, it should match with the token in the header
+                            // If not, an attacker has attached the header
+                            // The cookie cannot be attached by an attacker because it is HttpOnly
+
+                            await Task.CompletedTask;
+                        }
                     };
                 });
 
