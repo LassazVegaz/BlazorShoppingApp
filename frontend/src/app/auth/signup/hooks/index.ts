7c75ab61428c54f4fb1a43693666361ea28d63ee
LassazVegaz <lasindunuwanga@gmail.com>
2023-12-30 08:19:29 +0530

    navigate to login after signup

--- frontend/src/app/auth/signup/hooks/index.ts
+++ frontend/src/app/auth/signup/hooks/index.ts
@@ -5,9 +5,11 @@ import { useAppDispatch } from "@/redux/hooks";
 import { pageLoaderActions } from "@/redux/slices/page-loader.slice";
 import usersApi from "@/lib/users-api";
 import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
 const useSignUpUtils = () => {
   const dispatch = useAppDispatch();
+  const router = useRouter();
 
   const form = useForm({
     defaultValues: formDefaultValues,
@@ -21,6 +23,7 @@ const useSignUpUtils = () => {
           dateOfBirth: dateOfBirth!.format("YYYY-MM-DD"),
         });
         toast.success("Account created successfully");
+        router.push("/auth/signin");
       } catch (error) {
         toast.error("Sorry, something went wrong. Please try again later.");
         console.error(error);
