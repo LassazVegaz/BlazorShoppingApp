fd0667e4f1640e454a168af5c517b82a57c50a28
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-02 18:57:07 +0530

    show, update & reset email

--- frontend/src/app/auth/profile/components/ContactInfoForm.tsx
+++ frontend/src/app/auth/profile/components/ContactInfoForm.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { TextField, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import FormSection from "./FormSection";
 import { FormButton } from "./styled-components";
 import useUtils from "../hooks/contact-info-form.hook";
 import dayjs from "dayjs";
 import { useMemo } from "react";
+import { MuiTanTextField } from "@/components/MuiTanFields";
 
 const ContactInfoForm = () => {
   const { form, state, utils } = useUtils();
@@ -18,52 +19,58 @@ const ContactInfoForm = () => {
   }, [state.defaultEmailUpdateInterval, state.emailUpdateOn]);
 
   return (
-    <FormSection
-      title="Contact information"
-      isLoading={state.isLoading}
-      onSubmit={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        form.handleSubmit();
-      }}
-      buttons={
-        <>
-          <FormButton
-            type="reset"
-            color="secondary"
-            variant="contained"
-            onClick={utils.resetForm}
-          >
-            Reset
-          </FormButton>
-          <FormButton type="submit" variant="contained">
-            Save
-          </FormButton>
-        </>
-      }
-    >
-      <TextField label="Email" type="email" />
-
-      {!state.isLoading && (
-        <Typography variant="body1">
-          {canUpdateEmailIn < 1 ? (
-            <>
-              Once you change your email address, you will be able to change it
-              again after{" "}
-              <strong>{state.defaultEmailUpdateInterval} days</strong>.
-            </>
-          ) : (
-            <>
-              You will be able to change your email address after{" "}
-              <strong>
-                {canUpdateEmailIn} day{canUpdateEmailIn > 1 ? "s" : ""}
-              </strong>
-              .
-            </>
+    <form.Provider>
+      <FormSection
+        title="Contact information"
+        isLoading={state.isLoading}
+        onSubmit={(e) => {
+          e.preventDefault();
+          e.stopPropagation();
+          form.handleSubmit();
+        }}
+        buttons={
+          <>
+            <FormButton
+              type="reset"
+              color="secondary"
+              variant="contained"
+              onClick={utils.resetForm}
+            >
+              Reset
+            </FormButton>
+            <FormButton type="submit" variant="contained">
+              Save
+            </FormButton>
+          </>
+        }
+      >
+        <form.Field name="email">
+          {(field) => (
+            <MuiTanTextField field={field} label="Email" type="email" />
           )}
-        </Typography>
-      )}
-    </FormSection>
+        </form.Field>
+
+        {!state.isLoading && (
+          <Typography variant="body1">
+            {canUpdateEmailIn < 1 ? (
+              <>
+                Once you change your email address, you will be able to change
+                it again after{" "}
+                <strong>{state.defaultEmailUpdateInterval} days</strong>.
+              </>
+            ) : (
+              <>
+                You will be able to change your email address after{" "}
+                <strong>
+                  {canUpdateEmailIn} day{canUpdateEmailIn > 1 ? "s" : ""}
+                </strong>
+                .
+              </>
+            )}
+          </Typography>
+        )}
+      </FormSection>
+    </form.Provider>
   );
 };
 
