c4c474bfe251503fa00f3e8793d027179aadbc92
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-03 19:05:11 +0530

    removed support for Swasbuckle

--- backend/APIGateway/Program.cs
+++ backend/APIGateway/Program.cs
@@ -3,23 +3,11 @@ using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 // ocelot
 builder.Configuration.AddJsonFile("ocelot.json");
 builder.Services.AddOcelot();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseOcelot().Wait();
 
