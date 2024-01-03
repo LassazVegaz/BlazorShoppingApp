b988089ffd1bbc7fe981358e34d266bee2c5360e
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-03 13:02:54 +0530

    ef core tools and config in program.cs

--- backend/ItemsService/Program.cs
+++ backend/ItemsService/Program.cs
@@ -1,9 +1,20 @@
+using ItemsService.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// db context
+builder.Services.AddDbContext<ItemsServiceContext>(ops =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var version = builder.Configuration.GetValue<string>("MySqlVersion");
+    ops.UseMySql(connectionString, new MySqlServerVersion(version));
+});
 
 var app = builder.Build();
 
