573a47c4c9483d93c44cf71de86042c56f1b5ef4
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-03 11:03:30 +0530

    auth redirections

--- frontend/src/app/auth/signup/page.tsx
+++ frontend/src/app/auth/signup/page.tsx
@@ -1,7 +1,11 @@
 import AuthContainer from "@/components/AuthContainer";
 import { Form } from "./components";
+import serverAuth from "@/lib/server/server-auth";
+import { redirect } from "next/navigation";
 
-const SignUpPage = () => {
+const SignUpPage = async () => {
+  if (await serverAuth.isAuthenticated()) redirect("/auth/profile");
+
   return (
     <AuthContainer
       leftPanel={{
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,8 @@
+import serverAuth from "@/lib/server/server-auth";
 import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  if (await serverAuth.isAuthenticated()) redirect("/auth/profile");
+
   redirect("/auth/signin");
 }
