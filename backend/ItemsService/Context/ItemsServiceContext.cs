de273abd4a4fe89b65b4423b3ffdd7dd6e53d794
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-03 12:50:23 +0530

    item model and db context

---
+++ backend/ItemsService/Context/ItemsServiceContext.cs
@@ -0,0 +1,9 @@
+﻿using ItemsService.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ItemsService.Context;
+
+public class ItemsServiceContext(DbContextOptions<ItemsServiceContext> options) : DbContext(options)
+{
+    public DbSet<Item> Items { get; set; }
+}
