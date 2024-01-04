ca84ee1c2d0dcc63b81858fddf991b5b38bf842f
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-04 16:18:52 +0530

    nice comments

--- backend/packages/TrendingApp.Packages.Authentication/Constants.cs
+++ backend/packages/TrendingApp.Packages.Authentication/Constants.cs
@@ -3,5 +3,8 @@
 // These are contants exposed to the outside world. Some of them are reflections of internal constants.
 public class Constants
 {
+    /// <summary>
+    /// Name of the cookie used to store the JWT token
+    /// </summary>
     public const string CookieName = InternalConstants.CookieName;
 }
