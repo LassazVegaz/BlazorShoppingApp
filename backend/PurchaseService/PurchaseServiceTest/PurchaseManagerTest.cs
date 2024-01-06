dda466445d5e05b141eacb075c9e78a735b0c8a8
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-06 21:10:27 +0530

    test proj

---
+++ backend/PurchaseService/PurchaseServiceTest/PurchaseManagerTest.cs
@@ -0,0 +1,15 @@
+namespace PurchaseServiceTest;
+
+public class PurchaseManagerTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
