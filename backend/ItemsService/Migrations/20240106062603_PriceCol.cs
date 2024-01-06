d25bcf8ad29414257fae356d5039570df93d0258
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-06 11:56:46 +0530

    price col added

---
+++ backend/ItemsService/Migrations/20240106062603_PriceCol.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItemsService.Migrations
+{
+    /// <inheritdoc />
+    public partial class PriceCol : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Price",
+                table: "Items",
+                type: "double",
+                nullable: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Items");
+        }
+    }
+}
--- backend/ItemsService/Models/Item.cs
+++ backend/ItemsService/Models/Item.cs
@@ -4,4 +4,5 @@ public class Item
 {
     public int Id { get; set; }
     public string Name { get; set; } = null!;
+    public double Price { get; set; }
 }
