3a3e68589e384fb38a4b9d365ec0cfe72b18d5ad
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-06 14:05:56 +0530

    models

---
+++ backend/PurchaseService/Models/Item.cs
@@ -0,0 +1,8 @@
+﻿namespace PurchaseService.Models;
+
+public class Item
+{
+    public int Id { get; set; }
+    public double Price { get; set; }
+    public List<User> Users { get; set; } = [];
+}
