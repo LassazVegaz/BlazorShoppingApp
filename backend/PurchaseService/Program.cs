e8bc5369904aa85f9ce20a0155a7a551150ff8ea
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-06 14:18:52 +0530

    db context setup in program.cs

--- backend/PurchaseService/Program.cs
+++ backend/PurchaseService/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using PurchaseService;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+// db context
+builder.Services.AddDbContext<PurchaseServiceContext>(ops =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    var version = builder.Configuration.GetValue<string>("MySqlVersion");
+    ops.UseMySql(connectionString, new MySqlServerVersion(version));
+});
 
 
 var app = builder.Build();
