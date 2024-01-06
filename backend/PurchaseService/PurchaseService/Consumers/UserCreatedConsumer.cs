4b60a133061021c7a9096419820817fd131790de
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-07 00:25:07 +0530

    user created consumer

---
+++ backend/PurchaseService/PurchaseService/Consumers/UserCreatedConsumer.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using MassTransit;
+using PurchaseService.Models;
+using TrendingApp.Packages.Contracts;
+
+namespace PurchaseService.Consumers;
+
+public class UserCreatedConsumer(PurchaseServiceContext context, IMapper mapper) : IConsumer<UserCreated>
+{
+    private readonly PurchaseServiceContext _context = context;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task Consume(ConsumeContext<UserCreated> context)
+    {
+        var model = _mapper.Map<User>(context.Message);
+        await _context.Users.AddAsync(model);
+    }
+}
