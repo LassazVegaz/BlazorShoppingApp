e508517828b4d63caac26529db2d10ab3c340657
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-06 11:48:34 +0530

    test removed

--- backend/UsersService/ShoppingApp.LogicTest/Services/UsersServiceTest.cs
+++ backend/UsersService/ShoppingApp.LogicTest/Services/UsersServiceTest.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
 using UsersService.Core.Data;
-using UsersService.Core.Models;
 using UsersService.Core.Options;
-using LogicServices = UsersService.Logic.Services;
 
 namespace UsersService.LogicTest.Services;
 
@@ -28,23 +25,5 @@ internal class UsersServiceTest
     public void TearDown()
     {
         serviceProvider.Dispose();
-    }
-
-    [Test]
-    // just a simple test. For now, tests are not the focus of this course
-    public async Task EmailExistsTestAsync()
-    {
-        var context = serviceProvider.GetRequiredService<ShoppingAppContext>();
-        var userOptions = serviceProvider.GetRequiredService<IOptions<UserOptions>>();
-
-        context.Users.Add(new User { Email = "abcd@abcd.com" });
-        await context.SaveChangesAsync();
-
-        var usersService = new LogicServices.UsersService(userOptions, context);
-        Assert.Multiple(async () =>
-        {
-            Assert.That(await usersService.EmailExists("abcd@abcd.com"));
-            Assert.That(!await usersService.EmailExists("abcd2@abcd.com"));
-        });
     }
 }
