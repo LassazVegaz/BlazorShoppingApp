99fe17e9d2210fd2e6371dba6507eb1a0018ec67
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-06 21:06:26 +0530

    purchase manager

---
+++ backend/PurchaseService/Core/IPurchaseManager.cs
@@ -0,0 +1,6 @@
+﻿namespace PurchaseService.Core;
+
+public interface IPurchaseManager
+{
+    public Task Purchase(int userId, int itemId);
+}
---
+++ backend/PurchaseService/PurchaseManager.cs
@@ -0,0 +1,23 @@
+﻿using PurchaseService.Core;
+
+namespace PurchaseService;
+
+public class PurchaseManager(PurchaseServiceContext context) : IPurchaseManager
+{
+    private readonly PurchaseServiceContext _context = context;
+
+    public async Task Purchase(int userId, int itemId)
+    {
+        var user = await _context.Users.FindAsync(userId) ?? throw new ArgumentException($"User with id {userId} does not exist");
+        var item = await _context.Items.FindAsync(itemId) ?? throw new ArgumentException($"Item with id {itemId} does not exist");
+
+        if (user.Credits < item.Price)
+            throw new ArgumentException($"User with id {userId} does not have enough money to buy item with id {itemId}");
+
+        user.Credits -= item.Price;
+
+        user.Items.Add(item);
+
+        await _context.SaveChangesAsync();
+    }
+}
