0ab2febd6e237d7c53ff91bfa1dff08591ec6e84
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-08 11:19:30 +0530

    logging

--- backend/PurchaseService/PurchaseService/Consumers/ItemCreatedConsumer.cs
+++ backend/PurchaseService/PurchaseService/Consumers/ItemCreatedConsumer.cs
@@ -5,15 +5,21 @@ using TrendingApp.Packages.Contracts;
 
 namespace PurchaseService.Consumers;
 
-public class ItemCreatedConsumer(PurchaseServiceContext context, IMapper mapper) : IConsumer<ItemCreated>
+public class ItemCreatedConsumer(PurchaseServiceContext context, IMapper mapper, ILogger<ItemCreatedConsumer> logger)
+    : IConsumer<ItemCreated>
 {
     private readonly PurchaseServiceContext _context = context;
     private readonly IMapper _mapper = mapper;
+    private readonly ILogger<ItemCreatedConsumer> _logger = logger;
 
     public async Task Consume(ConsumeContext<ItemCreated> context)
     {
+        _logger.LogInformation("Item created even received: {id}", context.Message.Id);
+
         var model = _mapper.Map<Item>(context.Message);
         await _context.Items.AddAsync(model);
         await _context.SaveChangesAsync();
+
+        _logger.LogInformation("Item saved to database: {id}", context.Message.Id);
     }
 }
--- backend/PurchaseService/PurchaseService/Consumers/UserCreatedConsumer.cs
+++ backend/PurchaseService/PurchaseService/Consumers/UserCreatedConsumer.cs
@@ -5,15 +5,21 @@ using TrendingApp.Packages.Contracts;
 
 namespace PurchaseService.Consumers;
 
-public class UserCreatedConsumer(PurchaseServiceContext context, IMapper mapper) : IConsumer<UserCreated>
+public class UserCreatedConsumer(PurchaseServiceContext context, IMapper mapper, ILogger<UserCreatedConsumer> logger)
+    : IConsumer<UserCreated>
 {
     private readonly PurchaseServiceContext _context = context;
     private readonly IMapper _mapper = mapper;
+    private readonly ILogger<UserCreatedConsumer> _logger = logger;
 
     public async Task Consume(ConsumeContext<UserCreated> context)
     {
+        _logger.LogInformation("User created even received: {id}", context.Message.Id);
+
         var model = _mapper.Map<User>(context.Message);
         await _context.Users.AddAsync(model);
         await _context.SaveChangesAsync();
+
+        _logger.LogInformation("User saved to database: {id}", context.Message.Id);
     }
 }
