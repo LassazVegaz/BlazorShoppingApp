22f62a54bc439cfa4b5e7e7b8fd43017d5dd02cd
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-09 15:56:32 +0530

    some loggers

--- backend/PurchaseService/PurchaseService/PurchaseSaga.cs
+++ backend/PurchaseService/PurchaseService/PurchaseSaga.cs
@@ -13,6 +13,7 @@ internal record PurchaseState : SagaStateMachineInstance
 internal class PurchaseSaga : MassTransitStateMachine<PurchaseState>
 {
     private readonly IMapper _mapper;
+    private readonly ILogger<PurchaseSaga> _logger;
 
     public State SavingOrder { get; set; } = null!;
     public State DeductingCredits { get; set; } = null!;
@@ -21,25 +22,32 @@ internal class PurchaseSaga : MassTransitStateMachine<PurchaseState>
     public Event<OrderSavingFinished> OrderSavingFinished { get; set; } = null!;
     public Event<DeductingCreditsFinished> DeductingCreditsFinished { get; set; } = null!;
 
-    public PurchaseSaga(IMapper mapper)
+    public PurchaseSaga(IMapper mapper, ILogger<PurchaseSaga> logger)
     {
         _mapper = mapper;
+        _logger = logger;
 
         Initially(
             When(UserPlacedOrder)
+                .Then(context => _logger.LogInformation("UserPlacedOrder event consumed"))
                 .PublishAsync(ctx => ctx.Init<OrderSavingStarted>(_mapper.Map<OrderSavingStarted>(ctx.Message)))
+                .Then(context => _logger.LogInformation("OrderSavingStarted event published"))
                 .TransitionTo(SavingOrder)
         );
 
         During(SavingOrder,
             When(OrderSavingFinished)
+                .Then(context => _logger.LogInformation("OrderSavingFinished event consumed"))
                 .PublishAsync(ctx => ctx.Init<DeductingCreditsStarted>(_mapper.Map<DeductingCreditsStarted>(ctx.Message)))
+                .Then(context => _logger.LogInformation("DeductingCreditsStarted event published"))
                 .TransitionTo(DeductingCredits)
         );
 
         During(DeductingCredits,
             When(DeductingCreditsFinished)
+                .Then(context => _logger.LogInformation("DeductingCreditsFinished event consumed"))
                 .Finalize()
+                .Then(context => _logger.LogInformation("Saga finished"))
         );
     }
 }
