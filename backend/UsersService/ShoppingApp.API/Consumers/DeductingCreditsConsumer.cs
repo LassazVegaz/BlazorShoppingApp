4def8ffe3c4fb0c486e4c65595dec146050a77e4
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-09 20:37:37 +0530

    deducting credits failed event publish

--- backend/UsersService/ShoppingApp.API/Consumers/DeductingCreditsConsumer.cs
+++ backend/UsersService/ShoppingApp.API/Consumers/DeductingCreditsConsumer.cs
@@ -1,25 +1,32 @@
-﻿using MassTransit;
+﻿using AutoMapper;
+using MassTransit;
 using TrendingApp.Packages.Contracts.Sagas.Order;
 using UsersService.Core.Services;
 
 namespace UsersService.API.Consumers;
 
-public class DeductingCreditsConsumer(ILogger<DeductingCreditsConsumer> logger, IUsersService usersService)
+public class DeductingCreditsConsumer(ILogger<DeductingCreditsConsumer> logger, IUsersService usersService, IMapper mapper)
     : IConsumer<DeductingCreditsStarted>
 {
     private readonly ILogger<DeductingCreditsConsumer> _logger = logger;
     private readonly IUsersService _usersService = usersService;
+    private readonly IMapper _mapper = mapper;
 
     public async Task Consume(ConsumeContext<DeductingCreditsStarted> context)
     {
         _logger.LogInformation("DeductingCreditsStarted event was received");
-        await _usersService.DeductCredits(context.Message.UserId, context.Message.Deduction);
-        _logger.LogInformation("Deducted user credits");
-
-        await context.Publish(new DeductingCreditsFinished
+        try
         {
-            CorrelationId = context.Message.CorrelationId,
-            UserId = context.Message.UserId,
-        });
+            await _usersService.DeductCredits(context.Message.UserId, context.Message.Deduction);
+            _logger.LogInformation("Deducted user credits");
+
+            await context.Publish(_mapper.Map<DeductingCreditsFinished>(context.Message));
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Deducting credits failed");
+            await context.Publish(_mapper.Map<DeductingCreditsFailed>(context.Message));
+        }
+
     }
 }
