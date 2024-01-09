5f217df6e80e86ca8de2a6875b4dfe393457dcd4
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-09 16:08:53 +0530

    DeductingCreditsStarted consumer

---
+++ backend/UsersService/ShoppingApp.API/Consumers/DeductingCreditsConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using TrendingApp.Packages.Contracts.Sagas.Order;
+using UsersService.Core.Services;
+
+namespace UsersService.API.Consumers;
+
+public class DeductingCreditsConsumer(ILogger<DeductingCreditsConsumer> logger, IUsersService usersService)
+    : IConsumer<DeductingCreditsStarted>
+{
+    private readonly ILogger<DeductingCreditsConsumer> _logger = logger;
+    private readonly IUsersService _usersService = usersService;
+
+    public async Task Consume(ConsumeContext<DeductingCreditsStarted> context)
+    {
+        _logger.LogInformation("DeductingCreditsStarted event was received");
+        await _usersService.DeductCredits(context.Message.UserId, context.Message.Deduction);
+        _logger.LogInformation("Deducted user credits");
+
+        await context.Publish(new DeductingCreditsFinished
+        {
+            CorrelationId = context.Message.CorrelationId,
+            UserId = context.Message.UserId,
+        });
+    }
+}
