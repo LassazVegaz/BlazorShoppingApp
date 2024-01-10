59c721e97e7a95fae61ffb9711ece1f12e461af7
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-10 11:23:08 +0530

    users manager

---
+++ backend/PurchaseService/PurchaseService/Managers/UsersManager.cs
@@ -0,0 +1,12 @@
+﻿using PurchaseService.Core;
+using PurchaseService.Models;
+
+namespace PurchaseService.Managers;
+
+public class UsersManager(PurchaseServiceContext context) : IUsersManager
+{
+    private readonly PurchaseServiceContext _context = context;
+
+
+    public async Task<User?> GetUser(int userId) => await _context.Users.FindAsync(userId);
+}
