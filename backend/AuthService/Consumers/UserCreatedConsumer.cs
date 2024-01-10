65841cfb5c27017428b8527c07231e04fa7c1253
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-10 20:33:59 +0530

    user created event consume

---
+++ backend/AuthService/Consumers/UserCreatedConsumer.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MassTransit;
+using TrendingApp.Packages.Contracts;
+
+namespace AuthService.Consumers;
+
+public class UserCreatedConsumer(ILogger<UserCreatedConsumer> logger, AuthServiceContext context, IMapper mapper)
+    : IConsumer<UserCreated>
+{
+    private readonly ILogger<UserCreatedConsumer> _logger = logger;
+    private readonly AuthServiceContext _context = context;
+    private readonly IMapper _mapper = mapper;
+
+    public async Task Consume(ConsumeContext<UserCreated> context)
+    {
+        _logger.LogInformation("UserCreated event received");
+        _logger.LogInformation("Creating user record: {id}", context.Message.Id);
+        await _context.Users.AddAsync(_mapper.Map<User>(context.Message));
+        await _context.SaveChangesAsync();
+        _logger.LogInformation("Created user record: {id}", context.Message.Id);
+    }
+}
