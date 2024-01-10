6ce77522b620465dc9371813fd20fd9a2a2c7e21
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-10 18:44:32 +0530

    removed old exceptions stuff

--- backend/UsersService/ShoppingApp.Core/Exceptions/BadArgumentsException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UsersService.Core.Exceptions;
-
-/// <summary>
-/// In a Web API, this exception can be considered as a 400 Bad Request.
-/// </summary>
-public class BadArgumentsException(string? message = null, object? data = null)
-    : CustomException(400, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.1", message, data)
-{
-}
