1f78d405ab33ff9b4f1c942e5dc39630f8c8753c
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-10 20:54:12 +0530

    auth manager

---
+++ backend/AuthService/AuthManager.cs
@@ -0,0 +1,24 @@
+﻿using AuthService;
+using AuthService.Core;
+using Microsoft.EntityFrameworkCore;
+using TrendingApp.Packages.Authentication.Services;
+using BC = BCrypt.Net.BCrypt;
+
+namespace UsersService.Logic.Services;
+
+public class AuthManager(AuthServiceContext context, ITokensService tokensService) : IAuthManager
+{
+    private readonly AuthServiceContext _context = context;
+    private readonly ITokensService _tokensService = tokensService;
+
+    public async Task<string?> Login(string email, string password)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        if (user is null) return null;
+
+        var passwordValid = BC.Verify(password, user.Password);
+        if (!passwordValid) return null;
+
+        return _tokensService.GenerateToken(user.Id.ToString());
+    }
+}
---
+++ backend/AuthService/Core/IAuthManager.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.Core;
+
+public interface IAuthManager
+{
+    /// <summary>
+    /// Login user with email and password. On success returns the JWT token otherwise null.
+    /// </summary>
+    /// <returns>If login is successful, returns the JWT token otherwise null.</returns>
+    Task<string?> Login(string email, string password);
+}
