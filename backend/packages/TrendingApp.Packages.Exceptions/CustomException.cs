dd23f4ff61596c9fcdad5ceb73b1b7e4395c87ec
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-10 17:33:34 +0530

    custom exceptions

---
+++ backend/packages/TrendingApp.Packages.Exceptions/CustomException.cs
@@ -0,0 +1,17 @@
+﻿namespace TrendingApp.Packages.Exceptions;
+
+/// <summary>
+/// The base exception created specially for Trending App. This exception aids web APIs specially. Includes properties
+/// such as <i>Url</i> and <i>HttpStatusCode</i>.
+/// </summary>
+public class CustomException(int? statusCode = null,
+                             string? url = null,
+                             string? message = null,
+                             object? data = null,
+                             Exception? innerException = null)
+    : Exception(message, innerException)
+{
+    public int? HttpStatusCode { get; private set; } = statusCode;
+    public string? Url { get; private set; } = url;
+    public new object? Data { get; private set; } = data;
+}
---
+++ backend/packages/TrendingApp.Packages.Exceptions/NotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace TrendingApp.Packages.Exceptions;
+
+// Thrown when an entity (or entities) is not found. In a Web API, this exception can be considered as a 404 Not Found.
+public class NotFoundException(string? message = null, object? data = null)
+    : CustomException(404, "https://datatracker.ietf.org/doc/html/rfc7231#section-6.5.4", message, data)
+{
+}
