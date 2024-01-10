38e2dbfdc5c5a7f050e09473d68bcd3a3febf3de
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-10 18:38:23 +0530

    cors package

---
+++ backend/packages/TrendingApp.Packages.Cors/Constants.cs
@@ -0,0 +1,6 @@
+﻿namespace TrendingApp.Packages.Cors;
+
+internal static class Constants
+{
+    public const string FrontEndUrl = "http://localhost:3000";
+}
---
+++ backend/packages/TrendingApp.Packages.Cors/Extension.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TrendingApp.Packages.Cors;
+
+public static class Extension
+{
+    public static IServiceCollection AddTrendingAppCors(this IServiceCollection services)
+        => services.AddCors(ops => ops.AddDefaultPolicy(policy => policy.WithOrigins(Constants.FrontEndUrl)
+                                                                        .AllowAnyHeader()
+                                                                        .AllowAnyMethod()
+                                                                        .AllowCredentials()));
+}
