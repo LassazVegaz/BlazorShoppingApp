0df61e24c096db2db68b02cc215cfc00b941eb78
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-10 18:14:02 +0530

    exception handler extension

---
+++ backend/packages/TrendingApp.Packages.ApiExceptionsHandler/Extension.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TrendingApp.Packages.ApiExceptionsHandler;
+
+public static class Extension
+{
+    public static IServiceCollection AddTrendingAppExceptionHandlers(this IServiceCollection services)
+        => services.AddExceptionHandler<AppExceptionsHandler>()
+                   .AddExceptionHandler<UnknownExceptionHandler>();
+}
