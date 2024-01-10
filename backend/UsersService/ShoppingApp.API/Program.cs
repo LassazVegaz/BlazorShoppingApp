589f0af14a34dd91631ae00bab8f3085fdc55b2d
LassazVegaz <lasindunuwanga@gmail.com>
2024-01-10 18:43:57 +0530

    use packages

--- backend/UsersService/ShoppingApp.API/Program.cs
+++ backend/UsersService/ShoppingApp.API/Program.cs
@@ -1,10 +1,9 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
+using TrendingApp.Packages.ApiExceptionsHandler;
 using TrendingApp.Packages.Authentication.Extensions;
+using TrendingApp.Packages.Cors;
 using TrendingApp.Packages.MassTransitDependencyInjection;
 using UsersService.API.Constants;
-using UsersService.API.ExceptionHandlers;
 using UsersService.API.Mapper;
 using UsersService.Core.Data;
 using UsersService.Core.Options;
@@ -19,33 +18,10 @@ builder.Services.AddControllers();
 builder.Services.Configure<UserOptions>(builder.Configuration.GetSection(OptionsNames.UserOptions));
 
 // cors
-builder.Services.AddCors(ops =>
-{
-    var frontEndUrl = builder.Configuration.GetValue<string>(ConfigurationKeys.FrontEndUrl)
-                      ?? throw new Exception("FrontEndUrl is not configured");
-    ops.AddDefaultPolicy(policy => policy.WithOrigins(frontEndUrl)
-                                         .AllowAnyHeader()
-                                         .AllowAnyMethod()
-                                         .AllowCredentials());
-});
+builder.Services.AddTrendingAppCors();
 
 // Exception handlers
-builder.Services.AddExceptionHandler<AppExceptionsHandler>();
-builder.Services.AddExceptionHandler<UnknownExceptionHandler>();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(ops =>
-{
-    ops.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
-    {
-        Type = SecuritySchemeType.Http,
-        Scheme = JwtBearerDefaults.AuthenticationScheme,
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme.",
-    });
-});
+builder.Services.AddTrendingAppExceptionHandlers();
 
 // authentication
 builder.Services.AddTrendingAppAuthentication();
@@ -71,12 +47,6 @@ builder.Services.AddScoped<IAuthService, Services.AuthService>();
 var app = builder.Build();
 
 app.UseExceptionHandler(ops => { });
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseCors();
 
